9819dfd434b93f19e68fe171df888dec46dbdef3
Bhushan <bhushan1810patil@gmail.com>
2021-04-01 23:23:45 +0530

    added neighbors methods

--- Cell.py
+++ Cell.py
@@ -17,4 +17,37 @@ class Cell:
         self.visited = False
         self.neighbors = list()
 
-        
+    def is_visited(self):
+        return self.visited
+
+    def visit(self):
+        self.visited = True
+
+    def get_row(self):
+        return self.row
+
+    def get_col(self):
+        return self.col
+
+    def get_dims(self):
+        return self.row, self.col
+
+    def add_neighbor(self, cell):
+        self.neighbors.append(cell)
+
+    def get_neighbor_count(self):
+        return len(self.neighbors)
+
+    def get_neighbor(self):
+        cell = None
+        random.shuffle(self.neighbors)
+
+        for spot in self.neighbors:
+            if not spot.is_visited():
+                cell = spot
+                break
+
+        return cell
+
+    def draw(self, win: pygame.Surface):
+        pass
