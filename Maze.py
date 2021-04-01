00c94aabe93f3ad8da327db31058876b5d2b714a
Bhushan <bhushan1810patil@gmail.com>
2021-04-01 23:57:29 +0530

    added maze init methods

--- Maze.py
+++ Maze.py
@@ -21,7 +21,33 @@ class Maze:
         self.clock = None
         self.win = None
         self.maze_win = None
-        
+        self.offsets = ((0, -1), (1, 0), (0, 1), (-1, 0))
+
+    def grid_init(self):
+        self.maze = list()
+        for row in range(self.rows):
+            self.maze.append(list())
+            for col in range(self.cols):
+                self.maze[row].append(Cell(row, col, self.size))
+
+    def is_valid_dims(self, row: int, col: int) -> bool:
+        return row in range(self.rows) and col in range(self.cols)
+
+    def update_neighbors(self):
+        for row in self.maze:
+            for cell in row:
+                r, c = row.get_dims()
+
+                for i, j in self.offsets:
+                    newR = r + i
+                    newC = c + j
+
+                    if self.is_valid_dims(newR, newC):
+                        newCell = self.maze[newR][newC]
+                        cell.add_neighbor(newCell)
+
+    def maze_init(self):
+        pass
 
     def run(self):
         pass
