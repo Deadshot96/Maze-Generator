7d6cdc2477b64208bf49dce54d009fb7c4e9e913
Bhushan <bhushan1810patil@gmail.com>
2021-04-01 23:44:38 +0530

    added constructor of Maze.py

--- Maze.py
+++ Maze.py
@@ -6,8 +6,23 @@ from settings import *
 class Maze:
     
     def __init__(self):
-        pass
-    
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.maze_width = MAZE_WIDTH
+        self.maze_height = MAZE_HEIGHT
+        self.xoff = XOFF
+        self.yoff = YOFF
+        self.fps = FPS
+        self.size = SIZE
+        self.rows = self.maze_height // self.size
+        self.cols = self.maze_width // self.size
+        self.font = None
+        self.maze = None
+        self.clock = None
+        self.win = None
+        self.maze_win = None
+        
+
     def run(self):
         pass
 
