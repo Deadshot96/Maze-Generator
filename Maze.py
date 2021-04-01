a9194bb3ddf11814cf72ff72ca6a7d1a7a67b1ae
Bhushan <bhushan1810patil@gmail.com>
2021-04-01 23:40:35 +0530

    created and added Maze.py

---
+++ Maze.py
@@ -0,0 +1,16 @@
+import random
+import pygame
+from Cell import Cell
+from settings import *
+
+class Maze:
+    
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    X = Maze()
+    X.run()
